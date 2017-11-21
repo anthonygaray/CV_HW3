dcd4841e55d5ad70b9686ea330b564e3b867997c
Anthony Garay <agaray1@jhu.edu>
2017-11-21 17:25:07 -0500

    First Commit

---
+++ p1a.py
@@ -0,0 +1,10 @@
+import torch
+import torchvision
+import torchvision.datasets as dset
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.autograd import Variable
+
