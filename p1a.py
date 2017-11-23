3091766245a176ed02b0112331a3642ef3c5b4fa
Anthony Garay <agaray1@jhu.edu>
2017-11-22 19:24:46 -0500

    Created Net

--- p1a.py
+++ p1a.py
@@ -8,3 +8,58 @@ import matplotlib.pyplot as plt
 import numpy as np
 from torch.autograd import Variable
 
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        # set up convolution layers
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 64, 5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, stride=2)
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 128, 5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2, stride=2)
+        )
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256),
+            nn.MaxPool2d(2, stride=2)
+        )
+
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(256, 512, 3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
+        )
+
+        self.layer5 = nn.Sequential(
+            nn.Linear(131072, 1024),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(1024)
+        )
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = out.view(out.size(0), -1)
+        out = self.layer5(out)
+        return out
+
+net = Net()
+print(net)
