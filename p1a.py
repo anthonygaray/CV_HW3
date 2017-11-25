99ea6d6332cf7bf0bbdc592c8af5af0dd0f4753e
Anthony Garay <agaray1@jhu.edu>
2017-11-25 01:08:43 -0500

    Tried to implement testing

--- p1a.py
+++ p1a.py
@@ -14,7 +14,7 @@ import sys
 
 # Hyper Parameters
 num_epochs = 5
-batch_size = 10
+batch_size = 100
 learning_rate = 0.01
 
 # LFW Dataset
@@ -22,8 +22,8 @@ train_dataset = LFW('train.txt', transform=transforms.Compose([transforms.Scale(
 test_dataset = LFW('test.txt', transform=transforms.Compose([transforms.Scale((128, 128)), transforms.ToTensor()]))
 
 # Data Loader (Input Pipeline)
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, num_workers=2, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, num_workers=2, shuffle=False)
 
 
 if (len(sys.argv) < 2):
@@ -47,29 +47,29 @@ elif(sys.argv[1] == '--save'):
         # Train the Model
         for epoch in range(num_epochs):
             for i, data in enumerate(train_loader):
-                img0, img1, label = data
-                img0 = Variable(img0).cuda()
+                img1, img2, label = data
                 img1 = Variable(img1).cuda()
+                img2 = Variable(img2).cuda()
                 label = Variable(label).cuda()
 
                 # Forward + Backward + Optimize
-                output1, output2 = net(img0, img1)
+                output1, output2 = net(img1, img2)
                 merged_out = torch.cat([output1, output2], 1)
 
-            last_layer = nn.Sequential(nn.Linear(2048, 1), nn.Sigmoid())
-            last_layer.cuda()
-            out = last_layer(merged_out)
+                last_layer = nn.Sequential(nn.Linear(2048, 1), nn.Sigmoid())
+                last_layer.cuda()
+                out = last_layer(merged_out)
 
-            optimizer.zero_grad()
-            loss = criterion(out, label)
-            loss.backward()
-            optimizer.step()
+                optimizer.zero_grad()
+                loss = criterion(out, label)
+                loss.backward()
+                optimizer.step()
 
-            if i % 10 == 0:
-                print("Epoch {}\n Current loss {}\n".format(epoch, loss.data[0]))
-                iteration_number += 10
-                counter.append(iteration_number)
-                loss_history.append(loss.data[0])
+                if i % 10 == 0:
+                    print("Epoch {}\n Current loss {}\n".format(epoch, loss.data[0]))
+                    iteration_number += 10
+                    counter.append(iteration_number)
+                    loss_history.append(loss.data[0])
 
         # show_plot(counter,loss_history)
 
@@ -86,27 +86,34 @@ elif (sys.argv[1] == '--load'):
         net = Net()
         net.load_state_dict(torch.load(sys.argv[2]))
 
-        # Test the Model
-        # net.eval()    # Change model to 'eval' mode (BN uses moving mean/var).
-        # correct = 0
-        # total = 0
+        net.eval()
+        correct = 0
+        total = 0
+        thresh = 0.5
 
-        # for i, data in enumerate(test_loader):
-        #	img1, img2, label = data
-        #	img1 = Variable(img1).cuda()
-        #	img2 = Variable(img2).cuda()
-        #	out1, out2 = net(img0, img1)
+        for i, data in enumerate(test_loader):
+            img1, img2, label = data
+            img1 = Variable(img1).cuda()
+            img2 = Variable(img2).cuda()
+            out1, out2 = net(img1, img1)
 
-        #	_, predicted = torch.max(outputs.data, 1)
+            merged_out = torch.cat([out1, out2], 1)
 
-        # for images, labels in test_loader:
-        #    images = Variable(images).cuda()
-        #    outputs = cnn(images)
-        #    _, predicted = torch.max(outputs.data, 1)
-        #    total += labels.size(0)
-        #    correct += (predicted.cpu() == labels).sum()
+            last_layer = nn.Sequential(nn.Linear(2048, 1), nn.Sigmoid())
+            last_layer.cuda()
+            out = last_layer(merged_out)
 
-        # print('Test Accuracy of the model on the 10000 test images: %d %%' % (100 * correct / total))
+            for i, val in enumerate(out):
+                if (val >= thresh):
+                    val = 1
+                else:
+                    val = 0
+                
+                if (val == label[i]):
+                    correct += 1
+
+            total += label.size(0)
+            print('Test Accuracy of the model on the test images: %d %%' % (100 * correct / total))
 
     else:
         print ("Error: File name needed")
